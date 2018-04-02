ca753aa5a907f959ecbf95432c97f73ef27b0227
Andrii Korchak <korchakandrii@gmail.com>
2018-04-02 17:51:53 +0300

    Update BB_22_all_codes.r

--- BB_22_all_codes.r
+++ BB_22_all_codes.r
@@ -10,18 +10,17 @@
 ##################################################################
 
 ### Example 1 - reading text data in.
-df1<-read.table(header=TRUE,stringsAsFactors = FALSE,
-text ="
-  col1 col2 col3 
-                1 a T
-                2 b T
-                3 c F"
+dm<-read.table(header=TRUE,stringsAsFactors = FALSE,
+                text ="
+                SUBJID AGE POFL 
+                001 23 Y
+                002 45 Y
+                003 61 N"
 )
-### Structure of df1
-str(df1)
+### Structure of dm
+str(dm)
 ### Pronting the values
-df1
-
+dm
 
 ### Example 2 reading data from CSV file
 
@@ -59,116 +58,99 @@ csv_file[,56] %>% unique %>% sort
 
 ###  Example 2.2.1 ***;
 ### keep variables 
-df_k <- df1 %>% select( c("col1","col2"))
+df_k <- dm %>% select( c("SUBJID","AGE"))
 df_k
 ### drop variables 
-df_d <- df1 %>% select( -col3 )
+df_d <- dm %>% select( -POFL )
 df_d
 
 
-### keeping all columns between col1 and col3 ***;
-### keep col1-col3;
-df1_k_betw <- df1 %>% select(num_range("col", 1:3))
-df1_k_betw
+### keeping all columns between SUBJID and POFL ***;
+### keep SUBJID-POFL;
+dm_k_betw <- dm %>% select(SUBJID:POFL)
+dm_k_betw
 
-# variables starts with ;
-df1_k_st <- df1 %>% select(starts_with("col"))
-df1_k_st
+# variables starts with AGE ;
+dm_k_st <- dm %>% select(ends_with("AGE"))
+dm_k_st
 
 # variables rename ;
-df1_rename <- df1 %>%
-   rename("Column1" = col1 )
-df1_rename
-
+dm_rename <- dm %>%
+  rename("PATNUM" = SUBJID )
+dm_rename
 
 ### 2.3 ###
-df1_i <- df1 %>% filter(col3==1, col2 == "a")
-df1_i
+dm_i <- dm %>% filter(POFL=="Y", AGE > 21)
+dm_i
 
 ### R base syntax – subset function
-df_i <- subset( df1, col3 == 1)
+df_i <- subset( dm, POFL == "Y" & AGE > 21 )
 df_i
 
 
 ### Creating new Variables by mutate()
 ### mutate() function and creating new variables ;
-df1
-df_new_var <- df1 %>% mutate(  
-  n_2 = n()/2,
+dm
+df_new_var <- dm %>% mutate(  
+  N_ROWS = n(),
   col4 = if_else(row_number() >= n()/2
                  ,">= n/2","<n/2","missing"),
-  col1 = col1 + 0.03
-)
+  USUBJID = paste0("A00-",SUBJID)
+) %>% select(-SUBJID)
 df_new_var
 
 ### Sorting data with arrange
 ### Example 2.5.1 Sorting and 
 ### Saving into new data frame 
-df1_sort <- df1 %>% arrange( col3 )
-df1_sort
+dm_sort <- dm %>% arrange( POFL )
+dm_sort
 
 ### Example 2.5.2 Sorting and 
 ### Saving into new data frame 
-df1_sort_desc <- df1 %>% arrange( col3, desc(col1) )
-df1_sort_desc
+dm_sort_desc <- dm %>% arrange( POFL, desc(SUBJID) )
+dm_sort_desc
 
 
 ### Set & Merge & Join
 ### Grouping varibles and statistics in R;
-df2 <- read.table(header = TRUE,
+vs <- read.table(header = TRUE,
                   stringsAsFactors = FALSE,
                   text = "
-                  SUBJID TEST1 TEST2 PERIOD
-                  S1 10 NA 1	
-                  S1 8  7 2	
-                  S2 7 4 1	
-                  S2 5 3 2	
-                  ") %>%
-  mutate( TEST = rowMeans(.[,c("TEST1","TEST2")],na.rm=TRUE ) ) 
-df2
-
+                  SUBJID HT WT PERIOD
+                  001 180 NA 1	
+                  001 181 77 2	
+                  002 173 85 1
+                  002 173 83 2	
+                  ")
+vs
 ### summarise() function produces 1 value for each by_group()
-df2_s <- df2 %>%
+vs_s <- vs %>%
   group_by( SUBJID ) %>%
-  summarise(
-    TEST_N    = n( ),
-    TEST_MEAN = mean( TEST ,na.rm = TRUE),
-    TEST_SDT  = sd ( TEST ,na.rm = TRUE),
-    TEST_MIN  = min( TEST ,na.rm = TRUE),
-    TEST_MAX  = max( TEST ,na.rm = TRUE)
-  )     %>% ungroup() %>%  
-  mutate( TOT_TEST_MEAN = mean( TEST_MEAN ) )
-df2_s
-df2_s %>% as.data.frame
-
+  summarise_at(
+        vars(HT,WT),
+        funs( sum(!is.na(.)),
+             mean(., na.rm=TRUE),
+               sd(., na.rm=TRUE),
+              min(. ,na.rm = TRUE),
+              max(. ,na.rm = TRUE))) %>%
+  ungroup() 
+vs_s
 
 ### lag and lead functions;
-df2_lag_lead <- df2 %>% 
-  select(SUBJID, TEST1) %>% 
+vs_lag_lead <- vs %>% 
+  select(SUBJID, HT) %>% 
   group_by( SUBJID) %>%
   mutate( 
-    LAG = lag( TEST1 ),
-    LEAD = lead( TEST1 )
-  )
-df2_lag_lead
+    LAG = lag( HT ),
+    LEAD = lead( HT )
+  ) %>% ungroup()
+vs_lag_lead
 ### Note that lag() and lead() are working within groups;
 
-
 ### Joins;
-### Additional Information about subjects;
-df_add_info <- read.table(header = TRUE,
-                          stringsAsFactors = FALSE,
-                          text = "
-                          SUBJID  NAME 
-                          S1 Nick
-                          S2 Cate 
-                          S3 Josh 
-                          ");
-df_add_info
-
 ###
-df_test_name <- df2 %>%
-  inner_join(y = df_add_info,
-            by = c("SUBJID" = "SUBJID")) %>%
-  select(NAME, TEST )
-df_test_name
+advs <- vs %>%
+  inner_join(y = dm,
+             by = c("SUBJID" = "SUBJID")) %>%
+  select(SUBJID,POFL, HT, WT  )
+advs
